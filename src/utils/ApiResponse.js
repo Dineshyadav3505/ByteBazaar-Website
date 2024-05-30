f8e2e293a8274b9f4777630c307b77b153d14207
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-05-30 12:02:41 +0530

    api response handler

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statuCode, data, message = "Success"){
+        this.statusCode = statuCode;
+        this.data = data;
+        this.message = message;
+        this.success = statuCode < 400;
+    }
+}
