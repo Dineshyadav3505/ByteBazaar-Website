995b7b046f8fc002030a38d95719099e5499a2d2
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-05-30 12:03:09 +0530

    AsyncHandler fun

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,14 @@
+const asyncHandeler = (requestHandler)=> async (req, res, next) => {
+    try {
+        await requestHandler(req, res, next);
+        
+    } catch (error) {
+        res.status(error.code || 500).json({
+            status: false,
+            message: error.message,
+        });
+    }
+}
+
+
+export {asyncHandeler}
