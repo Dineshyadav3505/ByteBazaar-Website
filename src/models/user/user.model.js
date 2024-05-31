3aad9c07e4eb08d56a3ff679e0ebfee8d5a89a25
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-05-31 15:34:14 +0530

    add Bycrypt and AccessToken

--- src/models/user/user.model.js
+++ src/models/user/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 // User Schema
 const userSchema = new Schema({
@@ -25,9 +27,39 @@ const userSchema = new Schema({
         required: [true, "Password is required"], 
     },
     role: { 
-        type: String, 
+        type: String,         
         default: "user"  
     }
 }, { timestamps: true });
+
+
+// Bcrypt password hashing
+userSchema.pre("save", async function(next) {
+    try {
+        if (!this.isModified("password")) return next();
+
+        this.password = await bcrypt.hash(this.password, 10);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+})
+
+
+// Password verification
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        { 
+            _id: this._id ,
+            email: this.email,
+            phoneNumber: this.phoneNumber,
+            fullName: this.fullName
+        }, 
+        process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRE});
+};
 
 export const User = mongoose.model("User", userSchema);
