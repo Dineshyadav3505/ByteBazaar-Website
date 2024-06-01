ff95dd66045b02ef082e4285b217663956e1d5fa
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-06-01 13:29:07 +0530

    write login controller & route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,96 +1,104 @@
 import mongoose from "mongoose";
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/ApiError.js"
-import { User } from "../models/user.model.js";
-import {ApiResponse} from "../utils/ApiResponse.js"
-import jwt from "jsonwebtoken"
-
-
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import  {User } from "../models/user/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const option = {
-    httpOnly: true,
-    secure:true
-}
+  httpOnly: true,
+  secure: true
+};
+
+const generateAccessToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+
+    return { accessToken };
+
+  } catch (error) {
+    throw new ApiError(500, "Access Token generation failed");
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, password, email, phoneNumber } = req.body;
 
-    const {fullName, password, email, phoneNumber} = req.body
+  if ([password, email, phoneNumber].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-    if([password, email, phoneNumber].some((field) => field?.trim() === "")) {
-        throw new ApiError(400, "All fields are required");
-    }
+  const existedUser = await User.findOne({
+    $or: [{ phoneNumber }, { email }],
+  });
 
-    const existedUser = await User.findOne({
-        $or: [{ phoneNumber }, { email }],
-    });
+  if (existedUser) {
+    throw new ApiError(409, "User with email or Phone Number already exists");
+  }
 
-    if (existedUser) {
-        throw new ApiError(409, "User with email or Phone Number already exists");
-    }
+  const user = await User.create({
+    fullName,
+    phoneNumber,
+    email,
+    password,
+  });
 
-    const user = await User.create({
-        fullName,
-        phoneNumber,
-        email,
-        password,
-    });
+  const createdUser = await User.findById(user._id).select("-password");
 
-    const createdUser = await User.findById(user._id).select("-password");
+  if (!createdUser) {
+    throw new ApiError(500, "User not created");
+  }
 
-    if(!createdUser){
-        throw new ApiError(500, "User not created")
-    }
-
-    return res
+  return res
     .status(201)
     .json(
-        new ApiResponse(201, createdUser, "User created successfully")
+      new ApiResponse(201, createdUser, "User created successfully")
     );
-
-
 });
 
-const loginUser = asyncHandler(async (req , res)=>{
-    const { email, username, password } = req.body;
-    
-    if(!(username || email)){
-      throw new ApiError(400,"enter email id and username")
-    }
-    
-    const user = await User.findOne({
-      $or: [ {username} , {email} ]
-    })
-  
-    if(!user){
-      throw new ApiError(404, " user does not exist")
-    }
-  
-    const passwordcheck = await user.matchPassword(password)
-  
-    if(!passwordcheck){
-      throw new ApiError(401,"Invalid user credentials")
-    }
-  
-    const {accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
-  
-    const LoggedInUser = await User.findById(user._id).select("-password -refreshToken" )  
-    
-    return res
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, phoneNumber, password } = req.body;
+
+  if (!(phoneNumber || email)) {
+    throw new ApiError(400, "Enter email id and Phone Number to login");
+  }
+
+  const user = await User.findOne({
+    $or: [{ phoneNumber }, { email }]
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const passwordCheck = await user.isPasswordCorrect(password);
+
+  if (!passwordCheck) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const {accessToken} = await generateAccessToken(user._id);
+
+  const loggedUser = await User.findById(user._id).select("-password");
+
+  console.log("User logged In Successfully");
+  return res
     .status(200)
     .cookie("accessToken", accessToken, option)
-    .cookie("refreshToken", refreshToken, option)
     .json(
       new ApiResponse(
         200,
         {
-          user : LoggedInUser, accessToken, refreshToken
+          user: loggedUser,
+          accessToken
         },
         "User logged In Successfully"
       )
-    )
-})
+    );
+});
 
-export {
-    registerUser,
-    loginUser
-}
+export { 
+  registerUser, 
+  loginUser,
+  logoutUser
+};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 const router = Router()
 
 
 router.route("/register").post(registerUser)
 router.route("/logIn").post(loginUser)
+router.route("/logOut").get(logoutUser)
 
 export default router;
