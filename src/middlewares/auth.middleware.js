e9e86f6108f7a3f6e24829eeb4f529cf3ccdf494
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-06-01 14:52:59 +0530

    Add  authentication middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,12 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.model.js";
+import { User } from "../models/user/user.model.js";
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: '../.env'
+});
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
   try {
@@ -11,13 +16,13 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
           throw new ApiError(401, "Unauthorized request")
       }
   
-      const decodedToken = jwt.verify(token, "OTUlsdf_GvbjbdfbHYgz_knfgk6fhHkjns244dHgdfkjhHsl23jfGl2sdfujnJ12ghdfJH0")
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
   
-      const user = await User.findById(decodedToken?.id).select("-password -refreshToken")
+      const user = await User.findById(decodedToken?._id).select("-password")
   
       if (!user) {
           
-          throw new ApiError(401, "Invalid Access Token")
+          throw new ApiError(401, "Invalid Access  v Token")
       }
 
       req.user = user;
@@ -27,3 +32,4 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
   }
   
 })
+
