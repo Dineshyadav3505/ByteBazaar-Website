e8bdc99175c626b349a3faa0df13565180f40388
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-06-03 18:20:46 +0530

    product discount model

---
+++ src/models/product/productDiscount.model.js
@@ -0,0 +1,22 @@
+import mongoose,{Schema} from "mongoose";
+
+const productDiscountSchema = new Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    description: { 
+        type: String, 
+        required: true
+    },
+    discount: {
+        type: Number,
+        required: true,
+    }, 
+    active:{ 
+        type: Boolean,
+        default: true
+    },
+},{timestamps:true});
+
+export const ProductDiscount = mongoose.model("ProductDiscount", productDiscountSchema);
