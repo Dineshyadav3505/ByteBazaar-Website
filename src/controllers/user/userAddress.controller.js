c73f058414af92a9b5a56c85ae2336214031ba4b
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-06-06 17:02:46 +0530

    make user Addres update route & controller

--- src/controllers/user/userAddress.controller.js
+++ src/controllers/user/userAddress.controller.js
@@ -47,10 +47,54 @@ const userAddress = asyncHandler(async (req, res) => {
       .json(
         new ApiResponse(201, userAddress, "User address created successfully")
       );
+});
+
+const updateUserAddress = asyncHandler(async (req, res) => {
+    const { addressLine1, addressLine2, city, pincode, phoneNumber } = req.body;
+    const user = req.user; // Assuming req.user is defined
+    const addressId = req.params.addressId; // Changed from req.params._id
+  
+    if (!user) {
+      throw new ApiError(401, "Please login to update address");
+    }
+  
+  
+    if (![addressLine1, addressLine2, city, pincode, phoneNumber].some((field) => field)) {
+      throw new ApiError(400, "All fields are required");
+    }
+  
+    if (pincode.length !== 6) {
+      throw new ApiError(400, "Invalid pincode");
+    }
+  
+    if (phoneNumber.length !== 10) {
+      throw new ApiError(400, "Invalid phone number");
+    }
+  
+    const updatedAddress = await Address.findOneAndUpdate(
+      { _id: addressId, user: user._id },
+      { addressLine1, 
+        addressLine2, 
+        city, 
+        pincode, 
+        phoneNumber },
+      { new: true }
+    );
+  
+    if (!updatedAddress) {
+      throw new ApiError(404, "Address not found");
+    }
+  
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, updatedAddress, "User address updated successfully")
+      );
   });
 
 
 export { 
     userAddress,
+    updateUserAddress,
 
 };
--- src/routes/user/userAddress.route.js
+++ src/routes/user/userAddress.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { userAddress } from "../../controllers/user/userAddress.controller.js";
+import { userAddress, updateUserAddress, deleteUserAddress } from "../../controllers/user/userAddress.controller.js";
 import {verifyJWT} from "../../middlewares/auth.middleware.js";
 const router = Router()
 
 
 router.route("/userAddress").post(verifyJWT, userAddress)
+router.route("/userAddressUpdate/:addressId").post(verifyJWT, updateUserAddress);
+router.route("/userAddressDelete/:addressId").delete(verifyJWT, deleteUserAddress);
 
 
 export default router;
