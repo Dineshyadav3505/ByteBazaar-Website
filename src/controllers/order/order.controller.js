ea19a25e7d58f209dfb9c534f9f966daddd5d0b3
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-07-04 23:58:22 +0530

    Refactor order creation in order.controller.js

--- src/controllers/order/order.controller.js
+++ src/controllers/order/order.controller.js
@@ -9,37 +9,17 @@ import UserAddress from "../../models/user/userAddress.model.js";
 
 const createOrder = asyncHandler(async (req, res) => {
   try {
-    console.log("ff")
     const user = req.user;
-    const { productIds, userAddressId, grandTotal } = req.body;
-
-    // Validate the list of product IDs
-    const products = await Product.find({ _id: { $in: productIds } });
-
-    if (products.length !== productIds.length) {
-      throw new ApiError(404, 'One or more products not found');
-    }
-
-    // Validate the user address
-    const userAddress = await UserAddress.findById(userAddressId);
-
-    if (!userAddress) {
-      throw new ApiError(404, 'User address not found');
-    }
-
-    // Create the order
-    const order = new Order.create({
-      userId: user._id,
-      productId: productIds,
-      grandTotal,
-      userAddress: userAddress._id,
-    });
-
-    res.status(201).json(new ApiResponse(201, order, 'Order created successfully'));
+    const id = (user._id).toString();
+    console.log(id);
+    const { productId, grandTotal, userAddress } = req.params;
+    console.log(productId, grandTotal, userAddress);
+    
   } catch (error) {
-    console.error(error);
-    throw error;
+    console.log("error", error)
   }
+  res.status(200).json(new ApiResponse(200, {}, "Order created successfully"));
+
 });
 
 const getOrders = asyncHandler(async (req, res) => {
